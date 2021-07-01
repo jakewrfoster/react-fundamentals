9faebb70afdb283c12fa695144a2d26dd6d6679b
Jake Foster <jake@alley.co>
2021-07-01 12:41:08 -0400

    Exercise 05 - Extra Credit 1&2

--- src/exercise/05.js
+++ src/exercise/05.js
@@ -12,7 +12,10 @@ import '../box-styles.css'
 
 // 🐨 add a style prop to each of them as well so their background color
 // matches what the text says it should be as well as `fontStyle: 'italic'`
-const doStyles = (bgColor) => ({backgroundColor: bgColor});
+const doStyles = (bgColor) => ({backgroundColor: bgColor, fontStyle: 'italic'});
+const Box = ({size, color, children}) => (
+  <div className={`box box--${size}`} style={doStyles(color)}>{children}</div>
+);
 const smallBox = <div className="box box--small" style={doStyles('lightblue')}>small lightblue box</div>
 const mediumBox = <div className="box box--medium" style={doStyles('pink')}>medium pink box</div>
 const largeBox = <div className="box box--large" style={doStyles('orange')}>large orange box</div>
@@ -20,9 +23,10 @@ const largeBox = <div className="box box--large" style={doStyles('orange')}>larg
 function App() {
   return (
     <div>
-      {smallBox}
-      {mediumBox}
-      {largeBox}
+      <Box size="small" color="lightblue">small lightblue box</Box>
+      <Box size="medium" color="pink">medium pink box</Box>
+      <Box size="large" color="orange">large orange box</Box>
+      <Box>sizeless box</Box>
     </div>
   )
 }
